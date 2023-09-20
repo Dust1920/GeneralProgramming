102bf3841e9b0fde3cf6b0de4218c4d35dad7793
david <a21820888@gmail.com>
2023-09-20 11:43:43 -0700

    ws

--- seattle_weather.py
+++ seattle_weather.py
@@ -15,15 +15,38 @@ time = pd.to_datetime(seattle['date'])
 
 # Datos de prueba 
 days = 400
-T_test = T[:days]
-time_test = time[:days]
+T_test = np.array(T[:days])
+time_test = np.array(time[:days])
 
 
 plt.plot(time_test, T_test)
 plt.show()
 
-# Caso 1: h_1 = h_2 = h
+# Caso 1: h variable.
 
-h = 1
+T_test_r = np.roll(T_test, -1)
+T_increments = T_test_r - T_test
+T_increments = np.delete(T_increments, -1)
+print(T_increments)
+p = 0
+q = 0
+h_plus = []
+h_minus = []
+for i in T_increments:
+    if i>0:
+        p += 1
+        h_plus.append(i)
+    else:
+        q += 1
+        h_minus.append(i)
 
-r_walk = np.zeros(days)
+p_prob = p / days  # p
+q_prob = q / days  # q
+
+hpos = np.array(h_plus)
+hneg = np.array(h_minus)
+
+pmean = hpos.mean()  # h_1
+qmean = hneg.mean()  # -h_2
+
+print(p_prob, pmean, q_prob, qmean)
